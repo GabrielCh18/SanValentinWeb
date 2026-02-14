3ebd69232bc28948ad9c7a75847a3fc367087463
GabrielCh18 <egchamorrop@utn.edu.ec>
2026-02-14 08:56:06 -0500

    Agregar archivos de proyecto.

---
+++ SanValentinWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SanValentinWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
